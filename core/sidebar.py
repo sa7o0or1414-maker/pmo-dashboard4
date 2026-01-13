94b4a6212f503daca472cc2536727a5590b2ce07
sa7o0or1414-maker <sa7o0or1414@gmail.com>
2026-01-13 09:55:21 +0300

    Update sidebar.py

--- core/sidebar.py
+++ core/sidebar.py
@@ -12,14 +12,14 @@ def render_sidebar():
         background: rgba(255,255,255,.04);
         border: 1px solid rgba(255,255,255,.1);
         text-decoration: none;
+        color: inherit;
     }
     </style>
     """, unsafe_allow_html=True)
 
     st.sidebar.markdown("### Menu")
-
     st.page_link("pages/dashboard.py", label="Home")
-    st.sidebar.markdown("#### Admin")
+    st.sidebar.markdown("### Admin")
     st.page_link("pages/upload_data.py", label="Upload Data")
     st.page_link("pages/settings.py", label="Settings")
     st.page_link("pages/admin_login.py", label="Admin Login")
