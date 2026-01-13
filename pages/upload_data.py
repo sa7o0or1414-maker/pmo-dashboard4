3db94024283577f22d78dc0c0fffe473ccc0225f
sa7o0or1414-maker <sa7o0or1414@gmail.com>
2026-01-13 13:21:12 +0300

    Update upload_data.py

--- pages/upload_data.py
+++ pages/upload_data.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 st.set_page_config(page_title="رفع البيانات", layout="wide")
+from core.sidebar import render_sidebar
+render_sidebar()
 
 from core.ui import hide_streamlit_default_nav
 hide_streamlit_default_nav()
