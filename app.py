f1f683264747a09b9d4fbad73af4617b66eea5d3
sa7o0or1414-maker <sa7o0or1414@gmail.com>
2026-01-13 12:57:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import bootstrap  # noqa: F401
 
+from core.ui import hide_streamlit_default_nav
+hide_streamlit_default_nav()
 from core.config import ensure_defaults, load_config, apply_branding
 from core.sidebar import render_sidebar
 
