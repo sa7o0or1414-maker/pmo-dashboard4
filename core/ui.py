9a8ec9fecc666693ba617410dcf4223213a7aafa
sa7o0or1414-maker <sa7o0or1414@gmail.com>
2026-01-13 13:09:05 +0300

    Update ui.py

--- core/ui.py
+++ core/ui.py
@@ -4,12 +4,9 @@ def hide_streamlit_default_nav():
     st.markdown(
         """
         <style>
-        /* Hide Streamlit multipage default navigation */
         [data-testid="stSidebarNav"] {
             display: none !important;
         }
-
-        /* Hide Streamlit menu & footer */
         #MainMenu { visibility: hidden; }
         footer { visibility: hidden; }
         </style>
