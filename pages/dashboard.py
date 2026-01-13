8c5b30cc42effffe44985a1aea2acff43a2633ef
sa7o0or1414-maker <sa7o0or1414@gmail.com>
2026-01-13 14:41:54 +0300

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -3,12 +3,6 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.markdown("""
-<style>
-[data-testid="stSidebarNav"] { display: none; }
-</style>
-""", unsafe_allow_html=True)
-
 # =========================
 # تحميل البيانات من Session
 # =========================
