f72181ba3c4a52ce3ace3309c2cde5db049ea133
sa7o0or1414-maker <sa7o0or1414@gmail.com>
2026-01-13 14:37:11 +0300

    Update upload_data.py

--- pages/upload_data.py
+++ pages/upload_data.py
@@ -3,6 +3,11 @@ import pandas as pd
 
 st.title("⬆️ رفع البيانات")
 
+# حماية الصفحة
+if not st.session_state.get("logged_in", False):
+    st.error("🚫 هذه الصفحة مخصصة للأدمن فقط")
+    st.stop()
+
 file = st.file_uploader("ارفع ملف Excel", type=["xlsx", "xls"])
 
 if file:
