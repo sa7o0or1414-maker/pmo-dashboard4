456b8826379e34105edecf7295bc4bd8e1eaf40f
sa7o0or1414-maker <sa7o0or1414@gmail.com>
2026-01-13 13:31:06 +0300

    Update upload_data.py

--- pages/upload_data.py
+++ pages/upload_data.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="رفع البيانات", layout="wide")
-from core.sidebar import render_sidebar
-render_sidebar()
+st.set_page_config(
+    page_title="رفع البيانات",
+    layout="wide"
+)
 
 from core.ui import hide_streamlit_default_nav
 hide_streamlit_default_nav()
